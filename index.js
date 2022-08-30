1781b04acd698f393e0139c66c0e6b7be8bf2544
rochdy0 <rochdy0@gmail.com>
2022-08-30 16:23:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js')
+const { REST } = require('@discordjs/rest');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+const discord_token = require('./token.json').token
+
+client.login(discord_token);
+
+// Ajout des SlashCommands sur Discord
+const SlashData = [];
+const SlashCommands = [];
+const commandFiles = fs.readdirSync('./SlashCommands').filter(file => file.endsWith('.js'));
+client.commands = new Collection();
+for (let file of commandFiles) {
+    file = file.replace('.js', '')
+    const command = require(`./SlashCommands/${file}.js`);
+    SlashCommands[file] = command[file]
+    SlashData.push(command.data);
+}
+
+const rest = new REST({ version: '9' }).setToken(discord_token);
+
+(async () => {
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands('1014173396537462864', '586231907948429313'),
+            { body: SlashData });
+        console.log('Application SlashCommands chargées avec succès');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+// Ajout des SlashCommands sur Discord
+
+
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
